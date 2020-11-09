fcb323522bf477997a04ce14f0a44608c9ade9d5
alansoto31 <soto.alan@hotmail.com>
2020-11-09 16:25:20 -0600

    Add tests for the doend_syn method

--- spec/checks_spec.rb
+++ spec/checks_spec.rb
@@ -3,10 +3,9 @@
 require '../lib/checks'
 
 describe Checks do
+  let(:test) { Checks.new }
   describe '#paren_syn' do
-    let(:test) { Checks.new }
-
-    let(:file) { File.open('paren_test.rb') }
+    let(:file) { File.open('test_spec.rb') }
 
     let(:op_paren) { '(()' }
 
@@ -32,8 +31,7 @@ describe Checks do
     context 'Testing paren_syn method' do
       it 'Checks if the method read a file' do
         expect { test.paren_syn(file) }
-          .to output("\e[0;31;49mline:1 Lint/Syntax: unexpected token
-        (\e[0m\n\e[0;31;49mline:1 Lint/Syntax: unexpected token )\e[0m\n").to_stdout
+          .to output("\e[0;31;49mline:1 Lint/Syntax: unexpected token (\e[0m\n\e[0;31;49mline:1 Lint/Syntax: unexpected token )\e[0m\n").to_stdout
       end
 
       it 'Checks if the method catch an extra opening parentheses' do
@@ -48,25 +46,53 @@ describe Checks do
 
       it 'Checks if the method catch a closing parentheses before an opening parentheses' do
         expect { test.paren_syn(order_paren) }
-          .to output("\e[0;31;49mline:1 Lint/Syntax: unexpected token )
-          \e[0m\n\e[0;32;49mNo offenses\e[0m\n").to_stdout
+          .to output("\e[0;31;49mline:1 Lint/Syntax: unexpected token )\e[0m\n\e[0;32;49mNo offenses\e[0m\n").to_stdout
       end
 
       it 'Checks if the method catch extra parentheses in several lines' do
         expect { test.paren_syn(lines_paren) }
-          .to output("\e[0;31;49mline:1 Lint/Syntax: unexpected token )\e[0m\n
-          \e[0;31;49mline:2 Lint/Syntax: unexpected token (
-            \e[0m\n\e[0;31;49mline:3 Lint/Syntax: unexpected token (\e[0m\n").to_stdout
+          .to output("\e[0;31;49mline:1 Lint/Syntax: unexpected token )\e[0m\n\e[0;31;49mline:2 Lint/Syntax: unexpected token (\e[0m\n\e[0;31;49mline:3 Lint/Syntax: unexpected token (\e[0m\n").to_stdout
       end
 
       it 'Checks if the method avoid to catch parentheses in a string and in a regex' do
         expect { test.paren_syn(lines_w_str) }
-          .to output("\e[0;31;49mline:1 Lint/Syntax: unexpected token )
-          \e[0m\n\e[0;31;49mline:4 Lint/Syntax: unexpected token (\e[0m\n").to_stdout
+          .to output("\e[0;31;49mline:1 Lint/Syntax: unexpected token )\e[0m\n\e[0;31;49mline:4 Lint/Syntax: unexpected token (\e[0m\n").to_stdout
       end
 
       it 'Checks if the method works when the is no offenses' do
         expect { test.paren_syn(ok) }.to output("\e[0;32;49mNo offenses\e[0m\n").to_stdout
+      end
+    end
+  end
+
+  describe '#doend_syn' do
+    context '' do
+      let(:file) { File.open('test_spec.rb') }
+
+      let(:ending_keyword) do
+        'do
+        end
+        end'
+      end
+
+      let(:starting_keywords) do
+        'do
+        if
+        while
+        def
+        end'
+      end
+
+      it 'Checks if the method read a file' do
+        expect { test.doend_syn(file) }.to output("").to_stdout
+      end
+
+      it 'Checks if the method cath an extra end' do
+        expect { test.doend_syn(ending_keyword) }.to output("").to_stdout
+      end
+
+      it 'Checks if the method cath starting key words' do
+        expect { test.doend_syn(starting_keywords) }.to output("").to_stdout
       end
     end
   end
